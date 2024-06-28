4504a04e721b076edcd56aba612bfae09acfc16c
-g <-g>
2024-06-28 16:18:00 +0530

    New

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
