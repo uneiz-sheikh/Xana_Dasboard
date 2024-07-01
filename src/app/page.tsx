e42e5cfe0a4a9536fe58f11f0d705ca7e35bf12b
uneiz-sheikh <uneizsheikh719@gmail.com>
2024-07-01 13:43:57 +0530

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,94 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import "./style.scss";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className="main">
+        <div className="header">
+          <div className="navbar">
+            <div className="brand_logo">
+              <a href="https://xana.net/app">
+                <div className="logo_img">
+                  <img src="https://ik.imagekit.io/xanalia/xana/xana-logo-light.svg" /> 
+                </div>
+              </a>
+            </div>
+
+            <div className="navbar_nav" role="navigation">
+              <div className="navbar_menu">
+                <div className="nav_dropdown_list">
+                  <div className="nav_dropdown_items">Welcome</div>
+
+                  <div className="dropdown_container">
+
+                  </div>
+                </div>
+                <div className="nav_dropdown_list">
+                  <div className="nav_dropdown_items">App</div>
+                  <div className="dropdown_container">
+
+                  </div>
+                </div>
+                <div className="nav_dropdown_list">
+                  <div className="nav_dropdown_items">Summit</div>
+                </div>
+                <div className="nav_dropdown_list">
+                  <div className="nav_dropdown_items">$XETA</div>
+                </div>
+                <div className="nav_dropdown_list">
+                  <div className="nav_dropdown_items">Land</div>
+                </div>
+                <div className="nav_dropdown_list">
+                  <div className="nav_dropdown_items">Game</div>
+                </div>
+                <div className="nav_dropdown_list">
+                  <div className="nav_dropdown_items">NFT</div>
+                </div>
+                <div className="nav_dropdown_list">
+                  <div className="nav_dropdown_items">XANA-CHAIN</div>
+                </div>
+                <div className="nav_dropdown_list">
+                  <div className="nav_dropdown_items">DOA</div>
+                </div>
+                <div className="nav_dropdown_list">
+                  <div className="nav_dropdown_items">Download</div>
+                </div>  
+                <div className="nav_dropdown_list">
+                  <div className="nav_dropdown_items">News</div>
+                </div>                
+              </div>
+            </div>
+
+            <div className="search_bar">
+              <div className="search_icon">
+                <div className="icon_wrap">
+                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.848 12.752L9.978 8.882C10.679 7.953 11.1 6.801 11.1 5.55C11.1 2.49 8.611 0 5.55 0C2.49 0 0 2.49 0 5.55C0 8.611 2.49 11.1 5.55 11.1C6.801 11.1 7.953 10.679 8.882 9.978L12.752 13.848C12.903 13.999 13.102 14.076 13.3 14.076C13.498 14.076 13.696 14 13.848 13.848C14.151 13.545 14.151 13.055 13.848 12.752ZM1.1 5.55C1.1 3.096 3.097 1.1 5.55 1.1C8.004 1.1 10 3.097 10 5.55C10 8.003 8.004 10 5.55 10C3.096 10 1.1 8.004 1.1 5.55Z" fill="black" fill-opacity="0.8"></path></svg>
+                </div>
+              </div>
+
+              <div className="notification_bar">
+                <div className="bell_icon_dropdown">
+                  <div className="bell_icon">
+                   <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bell" className="svg-inline--fa fa-bell fa-w-14 align-middle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224 512c35.32 0 63.97-28.65 63.97-64H160.03c0 35.35 28.65 64 63.97 64zm215.39-149.71c-19.32-20.76-55.47-51.99-55.47-154.29 0-77.7-54.48-139.9-127.94-155.16V32c0-17.67-14.32-32-31.98-32s-31.98 14.33-31.98 32v20.84C118.56 68.1 64.08 130.3 64.08 208c0 102.3-36.15 133.53-55.47 154.29-6 6.45-8.66 14.16-8.61 21.71.11 16.4 12.98 32 32.1 32h383.8c19.12 0 32-15.6 32.1-32 .05-7.55-2.61-15.27-8.61-21.71z"></path></svg>
+                   <span className="indicator"></span>
+                  </div>
+                </div>
+
+                <div className="user_id_sec">
+                  <div className="user_detail">
+                    <div className="profile_name">Tool</div>
+                    <div className="profile_img">
+                      <img src="https://ik.imagekit.io/xanalia/xana/logoimg.svg" alt="#"/>
+                    </div>
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
