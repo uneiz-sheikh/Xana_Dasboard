23e6f231476c0c298e8184d3e750204ed77b4870
uneiz-sheikh <uneizsheikh719@gmail.com>
2024-07-03 17:54:10 +0530

    Graph

--- src/templates/dashboard/index.tsx
+++ src/templates/dashboard/index.tsx
@@ -12,12 +12,43 @@ import { Container, Row, Col } from 'react-bootstrap'
       </div>    
       <Container className='dashboard_conti'>
         <Row className='cont_row'>
-          <Col xxl={6} xl={7} className='col pe-3'>
+          <Col xxl={6} xl={7} md={12} className='col pe-3'>
             <div className="card_body">
               <div className="card-title">Total Visitors</div>
+              <div className="card_graph">
+                <div className="ratio_1">
+                  1
+                </div>
+                <div className="ratio_O">
+                  0
+                </div>
+                <div className="graph">
+                  <div className="line"></div>
+                </div>
+
+                <div className="grapg_day">
+                  <span>Mon</span>
+                  <span>Tue</span>
+                  <span>Wed</span>
+                  <span>Thur</span>
+                  <span>Fri</span>
+                  <span>Sat</span>
+                  <span>Sun</span>
+                </div>
+              </div>
+              <div className="graph_btn_sec">
+                <div className="gg_btn">
+                  <input type="radio" id="Monthly" name="drone" value="Monthly" checked/>
+                  <label className="mb-0">Monthly</label>
+                </div>
+                <div className="gg_btn">
+                  <input type="radio" id="Monthly" name="drone" value="Monthly" checked/>
+                  <label className="mb-0">Day</label>
+                </div>
+              </div>
             </div>
           </Col>
-          <Col xxl={6} xl={5} className='col ps-2'>
+          <Col xxl={6} xl={5} md={12} className='col ps-2'>
             <div className="card_body">
               {/* <Row className='count_row'>
                 <Col md={6} className='count_sec'></Col>
