ac36dc62c700af3fdfcaae6f89e0de59f831b3e3
uneiz-sheikh <uneizsheikh719@gmail.com>
2024-07-08 12:13:03 +0530

    Add Chat Area

--- src/templates/Chat/index.tsx
+++ src/templates/Chat/index.tsx
@@ -190,6 +190,43 @@ const Chat = () => {
                   <span className='time_tag2'>...</span>
                 </div>
               </div>
+
+              <div className="chat_area">
+                <div className="user_detail_sec">
+                  <div className="user_detail">
+                    <div className="user_img">
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-circle" className="svg-inline--fa fa-user-circle fa-w-16 align-middle user_Icon user_profile_icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" color="#00000080"><path fill="currentColor" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8.4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z"></path></svg>
+                    </div>
+                    <div className="user_name">
+                      <span>Visitor_1</span>
+                      <div className="u_c_d">                      
+                        <input type="checkbox" id="inProgress"/>
+                        <label className="mb-0 text-nowrap styles_filter_text">Smart Reply</label>                                    
+                      </div>
+                    </div>
+                  </div>                
+                </div>
+
+                <div className="text_area_sec">
+                  <a href='#' className='ticket_btn'>
+                    <img src="https://xana.net/avatarchat/app/_next/static/media/ticket.5dfba1a4.png" className='img1'/>
+                    <img src="https://xana.net/avatarchat/app/_next/static/media/ticketWhite.18543fa4.png" className='img2'/>
+                    Create a Ticket
+                  </a>
+
+                  <textarea className='text_field' placeholder='Say something'></textarea>
+                  <div className='send_icon'>
+                    <img src="https://xana.net/avatarchat/app/_next/static/media/mingcute_send-dis.c91594a4.svg" className="mic_icon"/>                    
+                  </div>
+
+                  <div className="emoji_btn">
+                    <img src="https://ik.imagekit.io/xanalia/xana/assets-global_emogi.svg"/>
+                  </div>
+                  <div className="add_btn">
+                    <img src="https://ik.imagekit.io/xanalia/xana/asset-global_plush.svg" />
+                  </div>
+                </div>
+              </div>
             </div>
           </Row>
        </Container>
